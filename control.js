ebc36bb76d71ad91b20c838b7ec98bf7a58a9235
DarkHeart527 <darkheart527s@gmail.com>
2016-12-21 13:21:33 -0800

    Update control.js

--- control.js
+++ control.js
@@ -1,1 +1,13 @@
-
+function checkSize() {
+ if (size == "tiny") {
+   
+ } else if (size == "small") {
+   
+ } else if (size == "medium") {
+   
+ } else if (size == "large") {
+   
+ } else {
+   
+ }
+}
